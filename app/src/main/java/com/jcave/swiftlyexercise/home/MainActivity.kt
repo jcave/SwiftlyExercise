4f1094b48ed169e8c9b7c755119eadeafda112d0
Jason Cave <jcave@cbtnuggets.com>
2021-02-02 09:32:38 -0800

    Setup layout manager, recyclerview, and coil for image display

--- app/src/main/java/com/jcave/swiftlyexercise/home/MainActivity.kt
+++ app/src/main/java/com/jcave/swiftlyexercise/home/MainActivity.kt
@@ -1,23 +1,39 @@
 package com.jcave.swiftlyexercise.home
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.jcave.swiftlyexercise.R
+import androidx.recyclerview.widget.GridLayoutManager
+import com.jcave.swiftlyexercise.databinding.ActivityMainBinding
+import com.jcave.swiftlyexercise.models.ItemResultsResponse
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var mainActivityViewModel: MainActivityViewModel
+    private lateinit var productAdapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         mainActivityViewModel.productLiveData.observe(this, {
-            Log.i("MAIN_ACTIVITY", it.canvasUnit.toString())
+            updateView(it.managerSpecials)
         })
 
+        productAdapter = ProductAdapter(emptyList())
+        val productLayoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+
+        binding.recyclerview.apply {
+            adapter = productAdapter
+            layoutManager = productLayoutManager
+        }
+
+    }
+
+    private fun updateView(productList: List<ItemResultsResponse.ManagerSpecial>) {
+        productAdapter.update(productList)
     }
 }
--- app/src/main/java/com/jcave/swiftlyexercise/home/ProductAdapter.kt
+++ app/src/main/java/com/jcave/swiftlyexercise/home/ProductAdapter.kt
@@ -1,21 +1,59 @@
 package com.jcave.swiftlyexercise.home
 
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.jcave.swiftlyexercise.R
 import com.jcave.swiftlyexercise.models.ItemResultsResponse
 
-class ProductAdapter(var productList: List<ItemResultsResponse.ManagerSpecial>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ProductAdapter(var productList: List<ItemResultsResponse.ManagerSpecial>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
+        return ProductHolder(view)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        bindProduct(holder as ProductHolder, position)
+    }
+
+    private fun bindProduct(holder: ProductHolder, position: Int) {
+        val product = productList[position]
+
+        holder.apply {
+            price.text = product.originalPrice.toString()
+            salePrice.text = product.price.toString()
+            title.text = product.displayName
+
+            imgProduct.load(product.imageUrl) {
+                crossfade(true)
+            }
+
+            imgProduct.setImageURI(Uri.parse(product.imageUrl))
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return productList.count()
+    }
+
+    public fun update(productList: List<ItemResultsResponse.ManagerSpecial>){
+        this.productList = productList
+        notifyDataSetChanged()
+    }
+
+    class ProductHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val price: TextView = v.findViewById(R.id.text_price)
+        val salePrice: TextView = v.findViewById(R.id.text_sale_price)
+        val title: TextView = v.findViewById(R.id.text_title)
+        val imgProduct: ImageView = v.findViewById(R.id.image_item)
     }
 
 }
